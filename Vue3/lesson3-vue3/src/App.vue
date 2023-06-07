c5d9b1c6a6fbd62d66cda883e90819c277205e6b
QuanIT01 <n.h.quan126@gmail.com>
2023-06-07 20:34:23 +0700

    Component & Global Styles

--- Vue3/lesson3-vue3/src/App.vue
+++ Vue3/lesson3-vue3/src/App.vue
@@ -1,42 +1,34 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld ref="hello" msg="Welcome to Your Vue.js App" />
-  <header-app ref="header"></header-app>
-  <h1>Hello from vue cli</h1>
-  <button @click="onChange">Change me</button>
+  <!-- <HelloWorld ref="hello" msg="Welcome to Your Vue.js App" />
+  <header-app ref="header"></header-app> -->
+
+  <h1 class="fade">Hello from vue cli</h1>
+  <base-modal />
+  <!-- <button @click="onChange">Change me</button> -->
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-import HeaderApp from "./components/HeaderApp.vue";
+// import HelloWorld from "./components/HelloWorld.vue";
+// import HeaderApp from "./components/HeaderApp.vue";
+import ModalApp from "./components/ModalApp.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
-    HeaderApp: HeaderApp,
+    // HelloWorld,
+    // HeaderApp: HeaderApp,
+    BaseModal: ModalApp,
   },
   data() {
     return {};
   },
   methods: {
-    onChange() {
-      console.log("running here...");
-      console.log(this.$refs.hello);
-      console.log(this.$refs.header);
-      this.$refs.hello.onConsologTestting();
-    },
+    // onChange() {
+    //   console.log("running here...");
+    //   console.log(this.$refs.hello);
+    //   console.log(this.$refs.header);
+    //   this.$refs.hello.onConsologTestting();
+    // },
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
